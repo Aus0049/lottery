430b61149e7faaf8483c424b32dd261dc64d7ab8
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-02-07 13:04:24 +0800

    添加姓名

---
+++ src/components/ConstText.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Aus on 17/2/7.
+ */
+const ConstText = {
+    Classic: {
+        addNameNull: "姓名不能为空"
+    },
+    Common: {
+
+    }
+};
+
+export default ConstText;
--- src/containers/Classic/UserList.js
+++ src/containers/Classic/UserList.js
@@ -9,14 +9,23 @@ import {
     Stepper,
     StepLabel,
 } from 'material-ui/Stepper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import SwipeList from '../../components/SwipeList'
-import Tools from '../../components/Tools'
+import Tools from '../../components/Tools';
+import ConstText from '../../components/ConstText';
 
 class UserList extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            stepIndex: 0 // 当前进行的step数目
+            stepIndex: 0, // 当前进行的step数目
+            showDialog: true, // 是否显示对话框
+            addName: "" // 当前添加的用户名
         }
     }
     componentDidMount () {
@@ -25,8 +34,49 @@ class UserList extends React.Component {
     handleDeleteList (id) {
         console.log(id);
     }
+    handleChangeInput (e) {
+        let value = e.target.value;
+        this.setState({addName: value});
+    }
+    handleCheckAddName () {
+        let addName = this.state.addName;
+        // 检查name是否合法
+        if($.trim(addName).length == 0){
+          // 用户名输入为空
+
+        }
+    }
+    handleOpen () {
+
+    }
+    handleClose () {
+
+    }
     render () {
-        const {stepIndex} = this.state;
+        const {stepIndex, showDialog, addName} = this.state;
+        const actions = [
+            <FlatButton
+                label="取消"
+                primary={true}
+                onTouchTap={this.handleClose.bind(this)}
+            />,
+            <FlatButton
+                label="确定"
+                primary={true}
+                onTouchTap={this.handleClose.bind(this)}
+            />
+        ];
+        const DialogDOM = <Dialog
+                title="添加参与者姓名"
+                actions={actions}
+                modal={false}
+                open={showDialog}
+                className="add-dialog"
+                onRequestClose={this.handleClose.bind(this)}
+            >
+                <TextField className="add-input" hintText="姓名10字以内" value={addName} onChange={this.handleChangeInput.bind(this)}/>
+            </Dialog>;
+
         return (
             <MuiThemeProvider muiTheme={getMuiTheme({})}>
                 <div className="user-list-box">
@@ -42,6 +92,10 @@ class UserList extends React.Component {
                         <SwipeList key="1" title="zby11" id="1" onDelete={this.handleDeleteList} />
                         <SwipeList key="2" title="zby12" id="2" onDelete={this.handleDeleteList} />
                     </ul>
+                    <FloatingActionButton className="add-btn" onTouchTap={this.handleOpen.bind(this)}>
+                        <ContentAdd />
+                    </FloatingActionButton>
+                    {DialogDOM}
                 </div>
             </MuiThemeProvider>
         )
